4741c4da2d727e80d9a56e00e60866f3dae077f2
Fabián Morag <fabianmorag0405@gmail.com>
2025-11-29 18:44:41 -0300

    Remove duplicate import of CatsModule in app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './routes/cats/cats.module';
-import { CatsModule } from './routes/cats/cats.module';
 
 @Module({
   imports: [CatsModule],
